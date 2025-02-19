74df8d46b60a509fd34e792bf53d1a38a110d15e
Noh-U <unohunoh12@gmail.com>
2025-02-19 19:05:57 +0900

    백준 단계별 문제풀이 - 입출력과 사칙연산 추가

--- sts_printfscanf/sts_printfscanf/01_sts_printfscanf_2557.c
+++ sts_printfscanf/sts_printfscanf/01_sts_printfscanf_2557.c
@@ -1,5 +1,5 @@
 /*
-  [백준_문제_단계별로 풀어보기_1단계_입출력과 사칙연산_1단계_2557_Hello World_Hello World!를 화면에 출력하는 문제(예제 출력과 똑같이 출력해야 합니다.)
+  [백준_문제_단계별로 풀어보기_1단계_입출력과 사칙연산_1단계_2557_Hello World_Hello World!를 화면에 출력하는 문제(예제 출력과 똑같이 출력해야 합니다.)]
 
   문제
   Hello World!를 출력하시오.
@@ -19,7 +19,7 @@
 
 #include <stdio.h>
 
-int main(void)
+int main_1(void)
 {
 	printf("Hello World!\n");
 	return 0;
